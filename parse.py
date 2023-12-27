e590c131e9c6404b9f549ad40e32583267121a6b
Gauhar <asanbayg@gmail.com>
2023-12-27 23:26:05 +0600

    Update parser: handle hashtags and parse all available pages

--- parse.py
+++ parse.py
@@ -1,27 +1,46 @@
+import re
+import json
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://bilim-all.kz/quote/proverb"
+base_url = "https://bilim-all.kz/quote/proverb?page={}"
+total_pages = 496
 
-response = requests.get(url)
+maqal_mattel_list = []
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
-    blockquotes = soup.find_all("blockquote")
+for page_number in range(1, total_pages + 1):
+    url = base_url.format(page_number)
+    response = requests.get(url)
 
-    makal_matel_list = []
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        blockquotes = soup.find_all("blockquote")
 
-    for i, b in enumerate(blockquotes, 1):
-        makal_matel_text = b.text.strip()
-        # Split text into text and topics using "Ілмектер:" as a benchmark
-        text_part, topics_part = map(str.strip, makal_matel_text.split("Ілмектер:"))
-        topics = [topic.strip() for topic in topics_part.split(",")]
+        # Check if there are blockquotes just in case
+        if not blockquotes:
+            break
 
-        # Create a dictionary for the current makal matel
-        makal_matel_dict = {"text": text_part, "topics": topics}
-        makal_matel_list.append(makal_matel_dict)
+        for i, b in enumerate(blockquotes, 1):
+            maqal_mattel_text = b.text.strip()
+            # Split text into text and topics using "Ілмектер:" as a benchmark
+            text_part, topics_part = map(
+                str.strip, maqal_mattel_text.split("Ілмектер:")
+            )
+            # Split at both # and comma
+            topics = re.split(r"#|,", topics_part)
+            topics = [topic.strip() for topic in topics if topic]
 
-    for saying in makal_matel_list:
-        print(saying)
-else:
-    print(f"Failed to retrieve the page. Status code: {response.status_code}")
+            # Create a dictionary for the current maqal mattel
+            maqal_mattel_dict = {"text": text_part, "topics": topics}
+            maqal_mattel_list.append(maqal_mattel_dict)
+    else:
+        print(
+            f"Failed to retrieve the page number {page_number}. Status code: {response.status_code}"
+        )
+        break
+
+# Save the list to a JSON file
+with open("maqal_mattel_data.json", "w", encoding="utf-8") as json_file:
+    json.dump(maqal_mattel_list, json_file, ensure_ascii=False, indent=2)
+
+print(f"Data successfully saved to 'maqal_mattel_data.json' from {total_pages} pages.")
